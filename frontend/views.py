85f73dc6637662f2ddc5fa66a10edb2a12fcc512
Sabyasachi Rakshit <sabyasachirakshitacs19@gmail.com>
2022-11-06 10:01:50 +0530

    added views, templates, updated url

--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
+
+def products(request):
+    products = Products.objects.all()
+    return render(request, 'products.html', {'products': products})
