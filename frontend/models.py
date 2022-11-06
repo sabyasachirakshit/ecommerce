9c3d0c2423db5f4ce703b05fe6493fb8acb44a55
Sabyasachi Rakshit <sabyasachirakshitacs19@gmail.com>
2022-11-06 09:51:25 +0530

    models created and registered

--- frontend/models.py
+++ frontend/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Products(models.Model):
+    product_name = models.CharField(max_length=500)
+    product_desc = models.TextField()
+    product_price = models.DecimalField(max_digits=6, decimal_places=2)
+    product_image = models.CharField(max_length=5000, null=True, blank=True)
